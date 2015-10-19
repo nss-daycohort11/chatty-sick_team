73b7e20362682a1cd6611ea5c584d261fdf44f88
Emma Beaton <emma.beaton@gmail.com>
2015-10-19 16:18:02 -0500

    finished most tasks

--- javascript.js
+++ javascript.js
@@ -7,6 +7,23 @@ var clearAllButton = document.getElementById("clear-all");
 var messageBoard = document.getElementById("message-board");
 var darkThemeCheckbox = document.getElementById("dark-theme");
 var largeTextCheckbox = document.getElementById("large-text");
+var namesButtons = document.getElementById("names");
+var mode = "create";
+var targetText = "";
+var users = {
+  names: ["Emma", "Blair", "Max", "Red", "Nathan"]
+};
+var thisName = "";
+
+//add users names to radio buttons
+var outputNames = "";
+for (var i = 0; i < users.names.length; i++) {
+	outputNames += "<input type='radio' name='users' value='"
+	outputNames += users.names[i];
+	outputNames += "'>";
+	outputNames += users.names[i];
+}
+namesButtons.innerHTML = outputNames;
 
 
 // make function for #clear-all button to clear lower div
@@ -18,18 +35,30 @@ var output = "";
 
 // assign message value + delete button to div
 function addMessageToBoard () {
-	// this line below clears the ouput so messages cleared before aren't concatenated and shown again
-	output = messageBoard.innerHTML;
-	output += "<div class='message-text'>";
-	output += "<p class='text'>";
-	output += messageInput.value;
-	output += "</p>";
-	output += "<button class='clear-message'>";
-	output += "Delete";
-	output += "</button>";
-	output += "</div>";
-	messageBoard.innerHTML = output;
-	messageInput.value = "";
+	if (mode === "create") {
+		// this line below clears the ouput so messages cleared before aren't concatenated and shown again
+		output = messageBoard.innerHTML;
+		output += "<div class='message-text'>";
+		output += "<b>";
+		output += thisName;
+		output += ":</b>";
+		output += "<p class='text'>";
+		output += messageInput.value;
+		output += "</p>";
+		output += "<button class='edit-message'>";
+		output += "Edit";
+		output += "</button>"
+		output += "<button class='clear-message'>";
+		output += "Delete";
+		output += "</button>";
+		output += "</div>";
+		messageBoard.innerHTML = output;
+		messageInput.value = "";
+	} else if (mode === "edit") {
+		targetText.innerHTML = messageInput.value;
+		targetText.classList.remove("disappear");
+		messageInput.value = "";
+	}
 }
 
 function darkTheme () {
@@ -47,35 +76,33 @@ darkThemeCheckbox.addEventListener("click", darkTheme);
 
 largeTextCheckbox.addEventListener("click", largeText);
 
-messageInput.addEventListener("keydown", function (event) {
+messageInput.addEventListener("keydown", function(event) {
 	if (event.keyCode === 13) {
 		addMessageToBoard();
 	}
 });
 
+
+
 document.querySelector("body").addEventListener("click", function(event) {
   console.log(event);
+  var thisElement = event.target;
   // handle any click on .clear-message button
-  if (event.target.className.toLowerCase() === "clear-message") {
+  if (thisElement.className.toLowerCase() === "clear-message") {
     console.log("You clicked on an delete button ");
-    //target button element
-    var thisElement = event.target;
-    console.log(thisElement);
     //target parent div
     thisElement.parentNode.classList.add("hide");
+  } else if (thisElement.className.toLowerCase() == "edit-message") {
+  	console.log("You click the edit button");
+  	mode = "edit";
+  	targetText = thisElement.previousSibling;
+  	messageInput.value = targetText.innerHTML;
+  	targetText.classList.add("disappear");
+  } else if (thisElement.type === "radio") {
+  	console.log(thisElement.value);
+  	thisName = thisElement.value;
   }
 });
-
-
-
-
-
-
-
-
-
-
-
 
 
 
